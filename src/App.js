79c875109b4b45af1bd54fa95ec1005de6542850
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-28 22:46:07 -0500

    Iported the newest component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CardComponent from './Components/CardComponent';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Hello from React</h1>
+			<CardComponent />
 		</div>
 	);
 }
