62c451a8e2959946163195d05e6baae21f49a1ac
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-28 23:14:18 -0500

    Completed the mobile version for the community component

---
+++ src/Components/Community.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Community = () => {
+	return (
+		<React.Fragment>
+			<div className='community__container'>
+				<h3 className='community__title'>Join our Community</h3>
+				<p className='community__guarantee'>
+					30-day, hassle-free money back guarantee
+				</p>
+				<p className='community__text'>
+					Gain access to our full library of tutorials along with expert
+					code reviews. Perfect for any developers who are serious about
+					honing their skills.
+				</p>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Community;
