bfb31a230e2f3a764611b49adf60832edd1629d2
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-29 23:30:40 -0500

    Imported the WhyUs component

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Community from './Community';
 import Subscription from './Subscription';
+import WhyUs from './WhyUs';
 
 function CardComponent() {
 	return (
 		<React.Fragment>
 			<div className='card__container'>
 				<Community />
-				<Subscription />
+				<div className='card__group'>
+					<Subscription />
+					<WhyUs />
+				</div>
 			</div>
 		</React.Fragment>
 	);
