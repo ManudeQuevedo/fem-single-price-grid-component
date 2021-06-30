21534c831a90ee236da438adaf2d2d24bd48b768
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-29 23:15:40 -0500

    Imported the subscription component

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Community from './Community';
+import Subscription from './Subscription';
 
 function CardComponent() {
 	return (
 		<React.Fragment>
 			<div className='card__container'>
 				<Community />
+				<Subscription />
 			</div>
 		</React.Fragment>
 	);
