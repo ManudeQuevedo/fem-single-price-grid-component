86a8e173bdd8ffec22d131f8e655cef2c6e3f906
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-29 23:30:58 -0500

    Populated the WhyUs component

---
+++ src/Components/WhyUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WhyUs = () => {
+	return (
+		<React.Fragment>
+			<div className='why__container'>
+				<h3>Why Us Component</h3>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum,
+					quod.
+				</p>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default WhyUs;
